f87db936fc7bbf57098c61dc5db749b634afdedd
Vivek Singh <vsvivek2006@gmail.com>
2026-09-18 23:23:26 +0530

    chore: clean standard robots.txt for Googlebot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,30 +3,10 @@ import { siteConfig } from "@/data/siteConfig";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-      {
-        userAgent: [
-          "Googlebot",
-          "Bingbot",
-          "Slurp",
-          "DuckDuckBot",
-          "Baiduspider",
-          "YandexBot",
-          "Applebot",
-          "OAI-SearchBot",
-          "GPTBot",
-          "PerplexityBot",
-          "ClaudeBot",
-          "Google-Extended",
-        ],
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: `${siteConfig.discoveryDomain}/sitemap.xml`,
-    host: siteConfig.discoveryDomain,
   };
 }
